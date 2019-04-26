9072df97fc2d6984bf6ed1a8e8475bcff27039dd
oxygen311 <alex39-09@mail.ru>
2019-04-26 23:46:12 +0300

    Add bash environment

---
+++ run_estimate_grimm.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+PYTHONPATH=. python src/real_data_est.py grimm $1 $2 $3
+
---
+++ run_estimate_infercars.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+PYTHONPATH=. python src/real_data_est.py infercars $1 $2 $3 $4
