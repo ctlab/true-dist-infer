ec050b1570204655f6836d1f7395bcd9d1a1bb33
Alexey Zabelkin <alexeyzabelkin@yandex.ru>
2022-09-08 16:51:47 +0300

    move figs and data to folders

--- code_generating.py
+++
@@ -1,5 +0,0 @@
-vs = range(50, 2001, 10)
-
-for v in vs:
-    print(f'cp -r ucsc_7_working ucsc_7_diff_res/{v}k')
-    print(f'sed -i -e \'s/50000/{v * 1000}/g\' ucsc_7_diff_res/{v}k/config.file')
--- src/tree/test_r_2.py
+++ src/tree/test_r_2.py
@@ -1,4 +1,3 @@
-import rfpimp
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
